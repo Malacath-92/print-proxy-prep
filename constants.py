72dca5510895657d76ab2ab0a57c8ec96644e3b0
Mal <vingine.dev@gmail.com>
2024-09-05 16:19:40 +0200

    Use non-depracated page sizes

--- constants.py
+++ constants.py
@@ -1,12 +1,11 @@
 import os
-import typing
 import configparser
 
-from reportlab.lib.pagesizes import letter, A4, A3, legal
+from reportlab.lib.pagesizes import LETTER, A4, A3, LEGAL
 
 cwd = os.path.dirname(__file__)
 
-page_sizes = {"Letter": letter, "A4": A4, "A3": A3, "Legal": legal}
+page_sizes = {"Letter": LETTER, "A4": A4, "A3": A3, "Legal": LEGAL}
 
 card_size_with_bleed_inch = (2.72, 3.7)
 card_size_without_bleed_inch = (2.48, 3.46)
