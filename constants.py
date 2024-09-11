25ce971d03c8301586b466ec3ea478e15d289698
Mal <vingine.dev@gmail.com>
2024-09-11 10:17:28 +0200

    Add support for A5 pages

--- constants.py
+++ constants.py
@@ -1,11 +1,11 @@
 import os
 import configparser
 
-from reportlab.lib.pagesizes import LETTER, A4, A3, LEGAL
+from reportlab.lib.pagesizes import LETTER, A5, A4, A3, LEGAL
 
 cwd = os.path.dirname(__file__)
 
-page_sizes = {"Letter": LETTER, "A4": A4, "A3": A3, "Legal": LEGAL}
+page_sizes = {"Letter": LETTER, "A5": A5, "A4": A4, "A3": A3, "Legal": LEGAL}
 
 card_size_with_bleed_inch = (2.72, 3.7)
 card_size_without_bleed_inch = (2.48, 3.46)
